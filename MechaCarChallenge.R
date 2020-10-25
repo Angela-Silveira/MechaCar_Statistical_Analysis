75a398ab518f3d4ad88d0d3cc98c52bf02fa1d87
Angela Silveira <AngelaRivi515@gmail.com>
2020-10-25 17:40:13 -0400

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,5 +9,10 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_table)) #Determine p-value and r-squared using summary
 
 sc_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) #Read in CSV
-total_summary <- sc_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
-lot_summary <- sc_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create summary table
+total_summary <- sc_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #Create summary table with mean,median,variance,SD
+lot_summary <- sc_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create same summary table with groupby Lot
+
+t.test(lot_summary$Mean,mu=mean(sc_table$PSI)) #ttest for PSI
+t.test(lot_summary$Mean,mu=mean(lot_summary$Manufacturing_Lot == "Lot1"))
+t.test(lot_summary$Mean,mu=mean(lot_summary$Manufacturing_Lot == "Lot2"))
+t.test(lot_summary$Mean,mu=mean(lot_summary$Manufacturing_Lot == "Lot3"))
