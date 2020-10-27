0e868a4b11fc9e7ed2511f34c7677b889701d5b7
Angela Silveira <AngelaRivi515@gmail.com>
2020-10-27 12:13:00 -0400

    updated file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,7 +12,10 @@ sc_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors =
 total_summary <- sc_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #Create summary table with mean,median,variance,SD
 lot_summary <- sc_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create same summary table with groupby Lot
 
-t.test(lot_summary$Mean,mu=mean(sc_table$PSI)) #ttest for PSI
-t.test(lot_summary$Mean,mu=mean(lot_summary$Manufacturing_Lot == "Lot1"))
-t.test(lot_summary$Mean,mu=mean(lot_summary$Manufacturing_Lot == "Lot2"))
-t.test(lot_summary$Mean,mu=mean(lot_summary$Manufacturing_Lot == "Lot3"))
+t.test(lot_summary$Mean,mu=1500) #ttest for PSI
+Lot1 <- sc_table[which(sc_table$Manufacturing_Lot=="Lot1"),]
+t.test(Lot1$PSI,mu=1500)
+Lot2 <- sc_table[which(sc_table$Manufacturing_Lot=="Lot2"),]
+t.test(Lot2$PSI,mu=1500)
+Lot3 <- sc_table[which(sc_table$Manufacturing_Lot=="Lot3"),]
+t.test(Lot3$PSI,mu=1500)
